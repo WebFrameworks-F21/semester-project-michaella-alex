f4545cb63bb040a5aa187189c796134788e9c721
Alex <alexmiller965@gmail.com>
2021-11-23 13:09:43 -0500

    Added cors headers

--- Racks/rackspace/admin.py
+++ Racks/rackspace/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Rack)
+admin.site.register(PatchPanel)
+admin.site.register(UPS)
+admin.site.register(JBOD)
+admin.site.register(Server)
+admin.site.register(Network)
+admin.site.register(NetworkCard)
 
 # Register your models here.
