367de63ae6a3f4470616458dd95c0ec574d63f3e
Michaella Magtibay <michaeltmagtibay@gmail.com>
2021-12-02 16:09:07 -0500

    create rest of states, handleSubmit(), createObject()

--- client-racks/src/forms/ObjectsForm.js
+++ client-racks/src/forms/ObjectsForm.js
@@ -1,16 +1,77 @@
 import React, { useState, useEffect } from "react";
 
-export default function ObjectsForm(props) {
+async function createObject(details, token) {
+  console.log(details);
+  // const response = await fetch("http://localhost:8000/racks/rackspace/", {
+  //   method: "POST",
+  //   mode: "cors",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     Authorization: `Token ${token}`,
+  //   },
+  //   body: JSON.stringify(details),
+  // });
+
+  // console.log(response);
+}
+
+export default function ObjectsForm({ token, user }) {
   const [name, setName] = useState("");
   const [size, setSize] = useState(0);
   const [visibility, setVisibility] = useState("Private");
-  const [objType, setObjType] = useState("Server");
+  const [objType, setObjType] = useState("");
+  const [rackLocation, setRackLocation] = useState();
+  const [rackPosition, setRackPosition] = useState();
+  const [ports, setPorts] = useState();
+  const [runTime, setRunTime] = useState();
+  const [watts, setWatts] = useState();
+  const [cpu, setCpu] = useState();
+  const [ram, setRam] = useState();
+  const [storage, setStorage] = useState(); //hdisk_size
+  const [graphics, setGraphics] = useState();
+  const [slots, setSlots] = useState();
+  const [outlets, setOutlets] = useState();
+  const [surgeProtection, setSurgeProtection] = useState(false);
+
+  const handleSubmit = async function (event) {
+    event.preventDefault();
+    let details = {};
+
+    if (objType === "server") {
+      details = { cpu, ram, storage, graphics };
+    } else if (objType === "patch-panel") {
+      details = { ports };
+    } else if (objType === "jbod") {
+      details = { slots, storage };
+    } else if (objType === "switch") {
+      details = { ports };
+    } else if (objType === "ups") {
+      details = { watts, runTime };
+    }
+
+    try {
+      const rack = await createObject(
+        {
+          user,
+          name,
+          size,
+          visibility,
+          ...details,
+        },
+        token
+      );
+
+      console.log(rack);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h3> Create a new Object </h3>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Object Name:
           <input
@@ -49,6 +110,7 @@ export default function ObjectsForm(props) {
               setObjType(e.target.value);
             }}
           >
+            <option value=""></option>
             <option value="server">Server</option>
             <option value="patch-panel">Patch Panel</option>
             <option value="jbod">JBODs</option>
@@ -60,9 +122,9 @@ export default function ObjectsForm(props) {
         <label for="rack">
           Rack Location:
           <select
-            value={objType}
+            value={rackLocation}
             onChange={(e) => {
-              setObjType(e.target.value);
+              setRackLocation(e.target.value);
             }}
           ></select>
         </label>
@@ -73,8 +135,8 @@ export default function ObjectsForm(props) {
             type="number"
             min={1}
             max={100}
-            value={size}
-            onChange={(e) => setSize(e.target.value)}
+            value={rackPosition}
+            onChange={(e) => setRackPosition(e.target.value)}
           />
         </label>
 
@@ -84,8 +146,8 @@ export default function ObjectsForm(props) {
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
+              value={ports}
+              onChange={(e) => setPorts(e.target.value)}
             />
           </div>
         )}
@@ -96,8 +158,8 @@ export default function ObjectsForm(props) {
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
+              value={runTime}
+              onChange={(e) => setRunTime(e.target.value)}
             />{" "}
             hours
           </label>
@@ -109,23 +171,82 @@ export default function ObjectsForm(props) {
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
+              value={watts}
+              onChange={(e) => setWatts(e.target.value)}
             />
             {" W"}
           </label>
         )}
 
+        {objType === "ups" && (
+          <label for="outlets">
+            Outlets:
+            <input
+              type="number"
+              min={1}
+              value={outlets}
+              onChange={(e) => setOutlets(e.target.value)}
+            />
+          </label>
+        )}
+
+        {objType === "ups" && (
+          <label for="surge-protection">
+            Surge Protection:
+            <input
+              type="radio"
+              value={true}
+              name="surge-protection"
+              onChange={(e) => setSurgeProtection(e.target.value)}
+            />
+            {"Yes"}
+            <input
+              type="radio"
+              value={false}
+              name="surge-protection"
+              onChange={(e) => setSurgeProtection(e.target.value)}
+            />
+            {"No"}
+          </label>
+        )}
+
         {objType === "server" && (
+          <label for="cpu">
+            CPU speed:
+            <input
+              type="number"
+              min={1}
+              value={cpu}
+              onChange={(e) => setCpu(e.target.value)}
+            />
+            {" GHz "}
+          </label>
+        )}
+
+        {objType === "server" && (
+          <label for="ram">
+            RAM capacity:
+            <input
+              type="number"
+              min={1}
+              value={ram}
+              onChange={(e) => setRam(e.target.value)}
+            />
+            {" GB"}
+          </label>
+        )}
+
+        {(objType === "server" || objType === "jbod") && (
           <label for="hdisk">
             Storage:
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-            />
-            {" TB"}
+              value={storage}
+              onChange={(e) => setStorage(e.target.value)}
+            />{" "}
+            {objType === "server" && " TB"}
+            {objType === "jbod" && " "}
           </label>
         )}
 
@@ -135,23 +256,22 @@ export default function ObjectsForm(props) {
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
+              value={graphics}
+              onChange={(e) => setGraphics(e.target.value)}
             />{" "}
             {" MHz"}
           </label>
         )}
 
-        {objType === "server" && (
-          <label for="network">
-            Network:
+        {objType === "jbod" && (
+          <label for="slots">
+            Disk Slots:
             <input
               type="number"
               min={1}
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
+              value={slots}
+              onChange={(e) => setSlots(e.target.value)}
             />
-            {" Mbps"}
           </label>
         )}
 
