0a16d1805f76f52d32cf7e490f94d740b6cb1ce5
Alex <alexmiller965@gmail.com>
2021-12-06 17:06:20 -0500

    Added signup password checking

--- client-racks/src/views/Signup.js
+++ client-racks/src/views/Signup.js
@@ -1,10 +1,54 @@
 import React from "react";
 import { useState } from "react";
 
-import { Link, Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+const score = 4
+
+const strength= {
+  1: "Insufficent",
+  2: "Insufficent",
+  3: "Good",
+  4: "Great"
+}
+
+class StrengthBar extends React.Component {
+  render() {
+    let password = this.props.password
+    let variations = {
+      'digits': /\d/.test(password),
+      'lowercase charcters': /[a-z]/.test(password),
+      'uppercase charcters': /[A-Z]/.test(password),
+      'special charcters': /\W/.test(password),
+  }
+  let score = Object.keys(variations).reduce(((total, key) => variations[key] + total), 0)
+  this.props.setStrength(score)
+  let bars = []
+  for (var i=0; i < score; i++) {
+    bars.push(<div className="bar"></div>)
+  }
+  const info = Object.keys(variations).map((key, test) =>
+  <li>{variations[key] === true ? '✔️' : '❌'} has {key}</li>
+  )
+    return (
+      <div>
+        <p>{strength[score]} Strength</p>
+        <br></br>
+        <div>
+        {bars}
+        </div>
+        <ul>
+          {info}
+        </ul>
+
+      </div>
+    )
+  }
+}
+
 
 async function createUser(credentials) {
-  return fetch("http://localhost:3000/signup", {
+  return fetch("http://localhost:8000/register/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -15,23 +59,35 @@ async function createUser(credentials) {
 
 export default function Login({ setToken }) {
   const [userName, setUserName] = useState("");
+  const [passwordStrength, setPasswordStrength] = useState(0);
   const [userPassword, setUserPassword] = useState("");
-  const [userPasswordAgain, setUserPasswordAgain] = useState("");
+  const [userPasswordAgain, setUserPasswordAgain] = useState(false);
+  const [redirect, setRedirect] = useState(false)
+  const [error, setError] = useState("")
 
   const handleSubmit = async function (event) {
     event.preventDefault();
     console.log(userName);
     console.log(userPassword);
-    const token = await createUser({
-      userName,
-      userPassword,
-    });
-    setToken(token);
+    if (userPassword !== userPasswordAgain) {
+      setError("The Passwords do not match")
+    }
+    else if (passwordStrength < 3){
+      setError("Password too weak")
+    }
+    else {
+      const token = await createUser({
+        "username": userName,
+        "password": userPassword,
+      });
+      setRedirect(true);
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        {error && <p>Error: {error}</p>}
         <label>
           Username:
           <input
@@ -56,7 +112,9 @@ export default function Login({ setToken }) {
             onChange={(e) => setUserPasswordAgain(e.target.value)}
           />
         </label>
+        <StrengthBar password={userPassword} setStrength={setPasswordStrength}/>
         <input type="submit" />
+        {redirect && <Navigate to="/login"/>}
       </form>
     </div>
   );
